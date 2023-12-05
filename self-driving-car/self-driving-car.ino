0b88dd89d08fc654a46aa783dea479d97c33799f
Peter Ocean <43334939+ocean0112@users.noreply.github.com>
2023-12-05 16:08:27 -0500

    update to self-driving-car.ino
    deleted commented lines of code

--- self-driving-car/self-driving-car.ino
+++ self-driving-car/self-driving-car.ino
@@ -83,7 +83,7 @@ void loop()
         leftMotor.run(Motor::MotorStop);
         rightMotor.run(Motor::MotorStop);
     }
-  }
+  } // end of IR Remote
   if (tmMeasure) {
     /* stop and scans*/
     
@@ -92,7 +92,7 @@ void loop()
     rightMotor.run(Motor::MotorStop);
     
     // perform measures on each direction (left, forward, right)
-    
+  
     spin.write(90);
     delay(1300);
     measurefront = us.measureIN();
@@ -177,10 +177,7 @@ void loop()
       }
     }
     else if(measurefront > measureleft && measurefront > measureright){ //if the front is clear
-      if (measureleft > 3 && measureright > 3){
-
-
-      }
+      if (measureleft > 3 && measureright > 3){ }
       else if (measureleft < 4 && measureright > measureleft){
       //if (measureright > measureleft){
         Serial.println("right...");
@@ -225,58 +222,6 @@ void loop()
       rightMotor.run(Motor::MotorStop);
 
     }// right fucntions
-
-    /*
-    else if (measureleft > measureright) {
-      Serial.println("left...");
-      leftMotor.setSpeed(10);
-      rightMotor.setSpeed(10);
-      rightMotor.run(Motor::MotorFoward);
-      leftMotor.run(Motor::MotorReverse);
-      delay(125);
-    }
-    else if (measureright > measureleft){
-      Serial.println("right...");
-      leftMotor.setSpeed(10);
-      rightMotor.setSpeed(10);
-      rightMotor.run(Motor::MotorReverse);
-      leftMotor.run(Motor::MotorFoward);
-      delay(125);
-    }
-    else{
-      leftMotor.run(Motor::MotorStop);
-      rightMotor.run(Motor::MotorStop);
-      delay(100000);
-    }
-    leftMotor.setSpeed(100);
-    rightMotor.setSpeed(100);
-
-
-    /*
-    if(measurefront < 1 && measureright < 1 && measureleft < 1 ){
-      leftMotor.run(Motor::MotorStop);
-      rightMotor.run(Motor::MotorStop);
-      delay(10000);
-    }
-    else{
-      if (measurefront > measureleft && measurefront > measureright){
-        Serial.println("foward...");
-        leftMotor.run(Motor::MotorFoward);
-         rightMotor.run(Motor::MotorFoward);
-         delay(DELAY);
-      } // go forward
-      else if (measurefront < 7 && measureright < 7 ) {
-        Serial.println("left...");
-        rightMotor.run(Motor::MotorFoward);
-        leftMotor.run(Motor::MotorReverse);
-        delay(50);
-      } // turn left
-      else if (measurefront < 7 && measureleft < 7) {
-        Serial.println("right...");
-        rightMotor.run(Motor::MotorReverse);
-        leftMotor.run(Motor::MotorFoward);
-        delay(5000);
-      } // turn right*/
       tmMeasure = false;
     }//end of else of main function
  // }
@@ -289,22 +234,3 @@ void loop()
     tmMeasure = true;
   }
 }
-/*
-// subjected to change
-ISR(TIMER1_OVF_vect) {
-  tmMeasure = true;
-  //TCNT1 = TIMER1_VAL;
-  TCNT1H = (TIMER1_VAL >> 8);
-  TCNT1L = (TIMER1_VAL & 0x00FF);
-}
-
-void setupTimer1() { 
-            // des     s   XTAL / scaler
-  //TCNT1 = TIMER1_VAL;
-  TCNT1H = (TIMER1_VAL >> 8);
-  TCNT1L = (TIMER1_VAL & 0x00FF);
-  TCCR1A = 0;    // normal mode
-  TCCR1B = 0x05; // 1024 prescaler
-  TIMSK1 = (1<<TOIE1);
-}
-*/
